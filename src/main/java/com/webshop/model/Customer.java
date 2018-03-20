f9b44e9abd326955eaf82bf3d36b015c419a869e
marti <martijn_bakker@upcmail.nl>
2018-03-20 13:09:35 +0100

    - added productResource implementation

---
+++ src/main/java/com/webshop/model/Customer.java
@@ -0,0 +1,9 @@
+package com.webshop.model;
+
+/**
+ * Created by marti on 20-3-2018.
+ */
+public class Customer {
+    int id;
+    Adres adres;
+}
---
+++ src/main/java/com/webshop/model/Product.java
@@ -0,0 +1,25 @@
+package com.webshop.model;
+
+/**
+ * Created by marti on 20-3-2018.
+ */
+public class Product {
+    int id, prijs;
+    String naam, omschrijving, catagorie;
+
+    public Product(int id, int prijs, String naam, String omschrijving, String catagorie) {
+        this.id = id;
+        this.prijs = prijs;
+        this.naam = naam;
+        this.omschrijving = omschrijving;
+        this.catagorie = catagorie;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getCatagorie(){
+        return catagorie;
+    }
+}
